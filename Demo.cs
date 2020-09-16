44597c0d62d05b15b7be5806a55e216e57da51a2
TB132946 <71388251+TB132946@users.noreply.github.com>
2020-09-16 14:16:05 +0100

    Update Demo.cs

--- Demo.cs
+++ Demo.cs
@@ -1,2 +1,23 @@
-using system
-Console.Writeline("This literally won't do anything because I need the complex stuff at the start");
+using System;
+
+// Create a Hello Hills Road program in GitHub
+
+namespace Hello
+
+{
+
+class HelloHillsRoad
+
+{
+
+static void Main()
+
+{
+
+System.WriteLine("Hello Hills Road!");
+
+}
+
+}
+
+}
